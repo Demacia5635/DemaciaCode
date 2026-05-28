7faccba627d15888068ad9a4d2a066a6c4b3c9ba
matan <diz.matan@gmail.com>
2026-05-28 18:19:55 +0300

    samting

--- src/main/java/frc/robot/trajectory/Circle.java
+++ src/main/java/frc/robot/trajectory/Circle.java
@@ -18,36 +18,6 @@ public class Circle {
         this.isLeftTurn = isLeftTurn;
     }
 
-<<<<<<< HEAD
-    private double getDeltaAngle(Translation2d start, Translation2d end) {
-        double startAngle = Math.atan2(start.getY() - center.getY(), start.getX() - center.getX());
-        double endAngle = Math.atan2(end.getY() - center.getY(), end.getX() - center.getX());
-        double deltaAngle = endAngle - startAngle;
-        if (isLeftTurn && deltaAngle < 0) {
-            deltaAngle += 2 * Math.PI;
-        } else if (!isLeftTurn && deltaAngle > 0) {
-            deltaAngle -= 2 * Math.PI;
-        }
-        return deltaAngle;
-    }
-
-    public double getArchDistance() {
-        return radius * getDeltaAngle(new Translation2d(), new Translation2d());
-    }
-
-    public double getHeading(Pose2d p1, Pose2d p2, double angle) {
-        Translation2d v = p1.getTranslation().plus(p2.getTranslation());
-        Translation2d Vector = new Translation2d(v.getAngle().getRadians() + getArchDistance(),Math.sqrt(-1));
-        return 2* angle +Vector.getAngle().getRadians();
-    }
-
-    public double getVelocity(double maxVelocity, double maxAcceleration, double distanceLeft, double currentVelocity, double finishVelocity) {
-        DemaciaTrapezoid trapezoid = new DemaciaTrapezoid(maxVelocity, maxAcceleration);
-        return trapezoid.calculate(distanceLeft, currentVelocity, finishVelocity);
-    }
- 
-}
-=======
     /**
      * Calculates the circle center for a given midpoint using the bisector of the angle
      * between the vectors midPoint->from and midPoint->to.
@@ -84,4 +54,3 @@ public class Circle {
         return angle;
     }
 }
->>>>>>> parent of 001daa0 (change after matan saw it)
